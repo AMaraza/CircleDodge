4125a15428d338d21475148d8012a3a4e400fb40
AMaraza <116380152+AMaraza@users.noreply.github.com>
2025-07-04 16:52:39 -0400

    Added WebAssembly version for itch

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import pygame
 import player
 import obstacle
 import math
+import asyncio
 from pygame.math import Vector2
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Circle Dodge")
 clock = pygame.time.Clock()
-running = True
-dt = 0
 
 class MAIN:
     def __init__(self):
@@ -30,9 +30,9 @@ class MAIN:
         self.score_text_rect.center = (100, 50)
         screen.blit(self.score_text, self.score_text_rect)
 
-    def update(self):
-        self.player_circle.move_player(dt)
-        self.obstacles.move_obstacles(dt)
+    def update(self, _dt):
+        self.player_circle.move_player(_dt)
+        self.obstacles.move_obstacles(_dt)
 
         if self.obstacles.check_collision(self.player_circle.spawn_location, self.player_circle.radius):
             if self.player_circle.score > self.high_score:
@@ -42,7 +42,7 @@ class MAIN:
             self.temp_score = 0
 
         else:
-            self.player_circle.score += 1 * dt
+            self.player_circle.score += 1 * _dt
 
         if self.player_circle.score >= self.temp_score + 20:
             self.obstacles.speed *= 1.3
@@ -50,16 +50,19 @@ class MAIN:
 
 main_game = MAIN()
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+async def main():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
+        dt = clock.tick(60) / 1000
+        main_game.update(dt)
+        screen.fill("black")
+        main_game.draw_elements()
+        pygame.display.update()
+        await asyncio.sleep(0)
+    pygame.quit()
 
-    screen.fill("black")
-    main_game.draw_elements()
-    pygame.display.update()
-    main_game.update()
-    dt = clock.tick(60) / 1000
-
-pygame.quit()
+asyncio.run(main())
